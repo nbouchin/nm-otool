68774e76af484f87d938869d00f7b4c857fc12f5
Nicolas BOUCHINET <nbouchin@student.42.fr>
2018-10-16 16:05:59 +0200

    Add  munmap and change dprintf to printf

--- srcs/nm.c
+++ srcs/nm.c
@@ -6,7 +6,7 @@
 /*   By: nbouchin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/16 10:34:00 by nbouchin          #+#    #+#             */
-/*   Updated: 2018/10/16 14:43:50 by nbouchin         ###   ########.fr       */
+/*   Updated: 2018/10/16 15:48:07 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,19 @@ void		print_symtab(t_load_command *load_command, t_mach_header *mach_header, t_n
 	while (++i < ((t_symtab_command*)load_command)->nsyms)
 	{
 		if ((symtab[i].n_type & N_STAB))
-			dprintf(2, "%-17llx %c %s\n", symtab[i].n_value, 'S', (char *)string_table + symtab[i].n_un.n_strx);
+			printf("%-17llx %c %s\n", symtab[i].n_value, 'S', (char *)string_table + symtab[i].n_un.n_strx);
 		else if ((symtab[i].n_type & N_TYPE) == N_UNDF)
-			dprintf(2, "%-17llx %c %s\n", symtab[i].n_value, 'U', (char *)string_table + symtab[i].n_un.n_strx);
+			printf("%-17llx %c %s\n", symtab[i].n_value, 'U', (char *)string_table + symtab[i].n_un.n_strx);
 		else if ((symtab[i].n_type & N_TYPE) == N_ABS)
-			dprintf(2, "%-17llx %c %s\n", symtab[i].n_value, 'A', (char *)string_table + symtab[i].n_un.n_strx);
+			printf("%-17llx %c %s\n", symtab[i].n_value, 'A', (char *)string_table + symtab[i].n_un.n_strx);
 		else if ((symtab[i].n_type & N_TYPE) == N_SECT)
-			dprintf(2, "%-17llx %c %s\n", symtab[i].n_value, 'T', (char *)string_table + symtab[i].n_un.n_strx);
+			printf("%-17llx %c %s\n", symtab[i].n_value, 'T', (char *)string_table + symtab[i].n_un.n_strx);
 		else if ((symtab[i].n_type & N_TYPE) == N_PBUD)
-			dprintf(2, "%-17llx %c %s\n", symtab[i].n_value, 'P', (char *)string_table + symtab[i].n_un.n_strx);
+			printf("%-17llx %c %s\n", symtab[i].n_value, 'P', (char *)string_table + symtab[i].n_un.n_strx);
 		else if ((symtab[i].n_type & N_TYPE) == N_INDR)
-			dprintf(2, "%-17llx %c %s\n", symtab[i].n_value, 'I', (char *)string_table + symtab[i].n_un.n_strx);
+			printf("%-17llx %c %s\n", symtab[i].n_value, 'I', (char *)string_table + symtab[i].n_un.n_strx);
 		else if ((symtab[i].n_type & N_EXT))
-			dprintf(2, "%-17llx %c %s\n", symtab[i].n_value, 'E', (char *)string_table + symtab[i].n_un.n_strx);
+			printf("%-17llx %c %s\n", symtab[i].n_value, 'E', (char *)string_table + symtab[i].n_un.n_strx);
 		nlist += 1;
 	}
 }
@@ -44,15 +44,13 @@ void		print_symtab(t_load_command *load_command, t_mach_header *mach_header, t_n
 t_nlist		*get_symtab(t_load_command *load_command, t_mach_header *mach_header)
 {
 	uint32_t			i;
-	uint32_t			nsyms;
 	t_nlist				*nlist;
 	t_nlist				*symtab;
 
 	i = -1;
-	nsyms = ((t_symtab_command*)load_command)->nsyms + 1;
 	nlist = (t_nlist *)((char *)mach_header + ((t_symtab_command*)load_command)->symoff);
-	symtab = malloc(nsyms);
-	while (++i != nsyms)
+	symtab = malloc(((t_symtab_command*)load_command)->nsyms);
+	while (++i < ((t_symtab_command*)load_command)->nsyms + 1)
 	{
 		symtab[i] = *nlist;
 		nlist += 1;
@@ -84,23 +82,28 @@ int		ft_nm(t_mach_header *mach_header)
 int		main(int argc, char **argv)
 {
 	int				fd;
-	unsigned int	nb_file;
+	int	nb_file;
 	t_stat			buf;
 	t_mach_header	*mach_header;
 
-	nb_file = argc;
-	while (--nb_file) 
+	nb_file = 0;
+	while (++nb_file < argc) 
 	{
-		printf("%s:\n", argv[nb_file]);
 		fd = open(argv[nb_file], O_RDONLY);
 		if (fstat(fd, &buf) == -1)
-			return (0);
+		{
+			printf("%s: No such file or directory.\n", argv[nb_file]);
+			(nb_file + 1 < argc) ? ft_putendl("") : 0;
+			continue ;
+		}
 		mach_header = mmap(NULL, buf.st_size, PROT_WRITE | PROT_READ, MAP_PRIVATE, fd, 0);
 		if (mach_header == MAP_FAILED)
 			return (0);
+		(argc > 2) ? printf("%s:\n", argv[nb_file]) : 0;
 		ft_nm(mach_header);
+		munmap(mach_header, buf.st_size);
 		close(fd);
-		ft_putendl("");
+		(nb_file + 1 < argc) ? ft_putendl("") : 0;
 	}
 }
 
