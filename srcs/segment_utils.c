a25717bff8d6b5f33ff2bf406001f8516c1576dd
Nicolas BOUCHINET <nbouchin@student.42.fr>
2018-10-22 13:50:29 +0200

    Update: modify segment utils

--- srcs/segment_utils.c
+++ srcs/segment_utils.c
@@ -6,7 +6,7 @@
 /*   By: nbouchin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/19 17:00:08 by nbouchin          #+#    #+#             */
-/*   Updated: 2018/10/19 17:52:50 by nbouchin         ###   ########.fr       */
+/*   Updated: 2018/10/22 13:48:45 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,25 @@
 
 t_section_64	*get_section(t_load_command *load_command, t_mach_header_64 *mach_header_64)
 {
-	unsigned long			i;
-	unsigned long	 		nsects;
+	short					i;
 	t_section_64			*sectab;
 	t_section_64		   	*section;
 	t_segment_command_64	*segment_command;
  
-	i = 0;
-	segment_command = (t_segment_command_64*)(load_command);// Casting command into segment_command.
-	nsects = segment_command->nsects;	
+	i = -1;
+	segment_command = (t_segment_command_64*)(load_command);
 	section = (t_section_64*)(segment_command + 1);
-	sectab = malloc(nsects * sizeof(t_section_64));
-	while (nsects)
+	sectab = malloc(segment_command->nsects * sizeof(t_section_64));
+	while (++i < (short)segment_command->nsects)
 	{
 		sectab[i] = *section;
 		if (mach_header_64->magic != MH_MAGIC)
 			section++;
 		else
 			section = (t_section_64*)((t_section*)section + 1);
-		nsects--;
-		i++;
 	}
-	i = 0;
-	nsects = segment_command->nsects;	
-	while (i != nsects)
-	{
+	i = -1;
+	while (++i < (short)segment_command->nsects)
 		printf("%s\n", sectab[i].sectname);
-		i++;
-	}
 	return sectab;
 }
