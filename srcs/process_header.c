f894ad818f7fbe20159ae88ccd7025abea563988
Nicolas BOUCHINET <nbouchin@e1r4p14.42.fr>
2018-10-23 15:26:02 +0200

    get_md_64 and 32 done

--- srcs/process_header.c
+++ srcs/process_header.c
@@ -6,56 +6,74 @@
 /*   By: nbouchin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/17 16:49:09 by nbouchin          #+#    #+#             */
-/*   Updated: 2018/10/23 12:58:38 by nbouchin         ###   ########.fr       */
+/*   Updated: 2018/10/23 15:24:59 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft_nm.h"
 
-int		process_header(t_mach_header_64 *mach_header_64, uint32_t magic)
+
+t_metadata	*get_metadata_64(t_mach_header_64 *mach_header_64)
 {
-	static int			pass = 0;
 	uint32_t			ncmds;
 	t_load_command		*load_command;
 	t_metadata			*metadata;
 
-	ncmds = 0;
-	load_command = NULL;
 	ncmds = mach_header_64->ncmds;
 	metadata = (t_metadata*)malloc(sizeof(t_metadata));
+	load_command = (t_load_command*)(mach_header_64 + 1);
+	while (--ncmds)
+	{
+		if (load_command->cmd == LC_SEGMENT_64)
+			metadata->sectab = get_section(load_command, mach_header_64, metadata);
+		else if (load_command->cmd == LC_SYMTAB)
+		{
+			metadata->symtab = get_symtab(load_command, mach_header_64);
+			print_symtab(load_command, mach_header_64, metadata);
+			(metadata->symtab) ? free(metadata->symtab) : 0;
+		}
+		load_command = (t_load_command*)((char*)load_command + load_command->cmdsize);
+	}
+	return metadata;
+}
+
+t_metadata	*get_metadata_32(t_mach_header_64 *mach_header_64)
+{
+	uint32_t			ncmds;
+	t_load_command		*load_command;
+	t_metadata			*metadata;
+
+	ncmds = mach_header_64->ncmds;
+	metadata = (t_metadata*)malloc(sizeof(t_metadata));
+	load_command = (t_load_command*)((t_mach_header*)mach_header_64 + 1);
+	while (--ncmds)
+	{
+		if (load_command->cmd == LC_SEGMENT)
+			metadata->sectab = get_section(load_command, mach_header_64, metadata);
+		else if (load_command->cmd == LC_SYMTAB)
+		{
+			metadata->symtab = get_symtab(load_command, mach_header_64);
+			print_symtab(load_command, mach_header_64, metadata);
+			(metadata->symtab) ? free(metadata->symtab) : 0;
+		}
+		load_command = (t_load_command*)((char*)load_command + load_command->cmdsize);
+	}
+	return (metadata);
+}
+
+int		process_header(t_mach_header_64 *mach_header_64, uint32_t magic)
+{
+	static int			pass = 0;
+	t_metadata			*metadata;
+
+	metadata = NULL;
 	if (is_64bits(magic))
 	{
 		pass = 1;
-		load_command = (t_load_command*)(mach_header_64 + 1);
-		while (--ncmds)
-		{
-			if (load_command->cmd == LC_SEGMENT_64)
-				metadata->sectab = get_section(load_command, mach_header_64, metadata);
-			else if (load_command->cmd == LC_SYMTAB)
-			{
-				metadata->symtab = get_symtab(load_command, mach_header_64);
-				print_symtab(load_command, mach_header_64, metadata);
-				(metadata->symtab) ? free(metadata->symtab) : 0;
-			}
-			load_command = (t_load_command*)((char*)load_command + load_command->cmdsize);
-		}
+		metadata = get_metadata_64(mach_header_64);
 	}
 	else if (is_32bits(magic) && pass == 0)
-	{
-		load_command = (t_load_command*)((t_mach_header*)mach_header_64 + 1);
-		while (--ncmds)
-		{
-			if (load_command->cmd == LC_SEGMENT)
-				metadata->sectab = get_section(load_command, mach_header_64, metadata);
-			else if (load_command->cmd == LC_SYMTAB)
-			{
-				metadata->symtab = get_symtab(load_command, mach_header_64);
-				print_symtab(load_command, mach_header_64, metadata);
-				(metadata->symtab) ? free(metadata->symtab) : 0;
-			}
-			load_command = (t_load_command*)((char*)load_command + load_command->cmdsize);
-		}
-	}
+		metadata = get_metadata_32(mach_header_64);
 	free(metadata);
 	return (1);
 }
