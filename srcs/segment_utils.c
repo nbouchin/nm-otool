24a1345de0ba55009a920cd0334f67b13f08d9c0
Nicolas BOUCHINET <nbouchin@e3r2p21.42.fr>
2018-11-07 10:37:11 +0100

    Update: otool is ok, have to print file name

--- srcs/segment_utils.c
+++ srcs/segment_utils.c
@@ -6,7 +6,7 @@
 /*   By: nbouchin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/19 17:00:08 by nbouchin          #+#    #+#             */
-/*   Updated: 2018/11/06 17:45:21 by nbouchin         ###   ########.fr       */
+/*   Updated: 2018/11/07 10:12:13 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ t_section_64	*alloc_sectab_64(t_section_64 *sectab, t_metadata *metadata,
 	old_size = metadata->nsect;
 	metadata->nsect += sc_64->nsects;
 	sectab = (sectab == NULL)
-	? ft_memalloc(sc_64->nsects * sizeof(t_section_64))
-	: sectab_realloc(sectab, metadata->nsect * sizeof(t_section_64), old_size);
+		? ft_memalloc(sc_64->nsects * sizeof(t_section_64))
+		: sectab_realloc(sectab, metadata->nsect * sizeof(t_section_64), old_size);
 	return (sectab);
 }
 
 t_section_64	*alloc_sectab(t_mach_header_64 const *mach_header_64,
-	t_section_64 *sectab, t_metadata *mdata, t_segment_command const *sc)
+		t_section_64 *sectab, t_metadata *mdata, t_segment_command const *sc)
 {
 	int32_t	old_size;
 
@@ -46,15 +46,15 @@ t_section_64	*alloc_sectab(t_mach_header_64 const *mach_header_64,
 	{
 		mdata->nsect += ft_swap_int32(sc->nsects);
 		sectab = (sectab == NULL)
-		? ft_memalloc(ft_swap_int32(sc->nsects) * sizeof(t_section_64))
-		: sectab_realloc(sectab, mdata->nsect * sizeof(t_section_64), old_size);
+			? ft_memalloc(ft_swap_int32(sc->nsects) * sizeof(t_section_64))
+			: sectab_realloc(sectab, mdata->nsect * sizeof(t_section_64), old_size);
 	}
 	else
 	{
 		mdata->nsect += sc->nsects;
 		sectab = (sectab == NULL)
-		? ft_memalloc(sc->nsects * sizeof(t_section_64))
-		: sectab_realloc(sectab, mdata->nsect * sizeof(t_section_64), old_size);
+			? ft_memalloc(sc->nsects * sizeof(t_section_64))
+			: sectab_realloc(sectab, mdata->nsect * sizeof(t_section_64), old_size);
 	}
 	return (sectab);
 }
@@ -108,12 +108,53 @@ void			print_section(t_mach_header_64 const *mach_header_64, t_section_64 *secti
 	i = 0;
 	address = (char*)section->addr;
 	section_string = (char *)mach_header_64 + section->offset;
-	ft_putendl("");
 	while (i < section->size)
 	{
 		if (i % 16 == 0)
 			ft_printf("\n%016lx\t", address);
 		ft_printf("%02x ", (uint8_t)*section_string);
+		address++;
+		section_string++;
+		i++;
+	}
+}
+
+void			print_section_32(t_mach_header_64 const *mach_header_64, t_section_64 *section)
+{
+	uint32_t	i;
+	char		*section_string;
+	uint32_t	address;
+
+	i = 0;
+	address = ((t_section*)section)->addr;
+	section_string = (char *)mach_header_64 + ((t_section*)section)->offset;
+	while (i < ((t_section*)section)->size)
+	{
+		if (i % 16 == 0)
+			ft_printf("\n%08lx\t", address);
+		ft_printf("%02x ", (uint8_t)*section_string);
+		address++;
+		section_string++;
+		i++;
+	}
+}
+
+void			print_big_section_32(t_mach_header_64 const *mach_header_64, t_section_64 *section)
+{
+	uint32_t	i;
+	char		*section_string;
+	uint32_t	address;
+
+	i = 0;
+	address = ft_swap_int32(((t_section*)section)->addr);
+	section_string = (char *)mach_header_64 + ft_swap_int32(((t_section*)section)->offset);
+	while (i < ft_swap_int32(((t_section*)section)->size))
+	{
+		if (i % 4 == 0)
+			ft_putchar(' ');
+		if (i % 16 == 0)
+			ft_printf("\n%08lx\t", address);
+		ft_printf("%02x", (uint8_t)*section_string);
 		address++;
 		section_string++;
 		i++;
@@ -135,8 +176,15 @@ t_section_64	*get_section(t_load_command const *load_command,
 		sectab[i] = *section;
 		if (!ft_strcmp(section->segname, "__TEXT") && !ft_strcmp(section->sectname, "__text"))
 		{
-			print_section(mach_header_64, section);
-			//ft_putendl(section->segname);
+			if (is_64bits(mach_header_64->magic))
+				print_section(mach_header_64, section);
+			else
+			{
+				if (mach_header_64->magic == MH_CIGAM)
+					print_big_section_32(mach_header_64, section);
+				else
+					print_section_32(mach_header_64, section);
+			}
 		}
 		if (is_64bits(mach_header_64->magic))
 			section++;
--- test.sh
+++ test.sh
@@ -5,12 +5,11 @@ RED="\033[0;31m"
 NC="\033[0m"
 
 for filename in /bin/* /sbin/* /usr/lib/* /usr/bin/* /usr/sbin/*; do
-    RESULT=$(diff <(nm -p $filename 2>&-) <(./ft_nm $filename 2>&-))
+    RESULT=$(diff <(otool -t $filename 2>&-) <(./ft_otool $filename 2>&-))
     if [[ $? -ne 0 ]]
     then
 	echo "[${RED}ko${NC}] -- $filename"
 	echo "$filename" >> fail.txt
-	sleep 1
     else
 	echo "[${GRN}ok${NC}] -- $filename"
     fi
